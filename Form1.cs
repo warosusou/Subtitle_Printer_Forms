c440bcbc0221ce9ceb7db66c12a2ee3acc094ae7
warosusou <warosusou@outlook.com>
2019-08-07 15:34:08 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subtitle_Printer
+{
+    public partial class Form1 : Form
+    {
+        ImeReadableTextBox textBox;
+        Color TextColor;
+        bool EQ;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox = new ImeReadableTextBox
+            {
+                Size = Reference_TextBox.Size,
+                Location = Reference_TextBox.Location,
+                Multiline = Reference_TextBox.Multiline,
+                Anchor = Reference_TextBox.Anchor,
+                Margin = Reference_TextBox.Margin,
+                Text = "ImeTextBox"
+            };
+            textBox.ImeCompositionHira += TextBox_ImeCompositionHira;
+            textBox.KeyDown += TextBox_KeyDown;
+            textBox.KeyPress += TextBox_KeyPress;
+            textBox.TextChanged += TextBox_TextChanged;
+            this.Controls.Add(textBox);
+            Reference_TextBox.Visible = false;
+            TextColor = textBox.SelectionColor;
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox.SelectionStart + "</Tex>".Length > textBox.Text.Length) { return; }
+            if (e.KeyChar == '\\' && textBox.Text[textBox.SelectionStart - 1] == '\\' && textBox.Text.Substring(textBox.SelectionStart, "</Tex>".Length) == "</Tex>" && EQ)
+            {
+                var pos = textBox.SelectionStart;
+                textBox.Text = textBox.Text.Remove(pos - 1, 1);
+                textBox.SelectionStart = pos;
+                e.Handled = true;
+                Leave_EQmode();
+                textBox.SelectionStart += "</Tex>".Length;
+            }
+            else if (e.KeyChar == '￥' && !EQ)
+            {
+                e.Handled = true;
+            }
+            else if(EQ)
+            {
+                var charactor = e.KeyChar;
+                e.Handled = true;
+                textBox.SelectedText = charactor.ToString();
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right && EQ)
+            {
+                /*if (textBox.SelectionStart < EQ_head || EQ_last < ime.SelectionStart)
+                {
+                    Leave_EQmode();
+                }*/
+            }
+        }
+
+        private void TextBox_ImeCompositionHira(object sender, ImeReadableTextBox.ImeCompositionEventArgs e)
+        {
+            if (e.InputString == "￥" && !EQ)
+            {
+                var pos = textBox.SelectionStart;
+                EQ = true;
+                textBox.ImeMode = ImeMode.Disable;
+                textBox.Text = textBox.Text.Insert(pos, "<Tex></Tex>");
+                textBox.Text = textBox.Text.Remove(pos + "<Tex></Tex>".Length, 1);
+                textBox.SelectionStart = pos + "<Tex>".Length;
+                textBox.SelectionColor = Color.Red;
+                this.Text += " - EQ mode";
+            }
+        }
+        private void Leave_EQmode()
+        {
+            EQ = false;
+            textBox.SelectionLength = 0;
+            textBox.SelectionColor = TextColor;
+            textBox.ImeMode = ImeMode.Hiragana;
+            this.Text = "Form1";
+        }
+    }
+}
---
+++ IME_Readable_TextBox.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Subtitle_Printer
+{
+    public class ImeReadableTextBox : RichTextBox
+    {
+        public delegate void ImeCompositionEventHandler(object sender, ImeCompositionEventArgs e);
+
+        public event ImeCompositionEventHandler ImeCompositionHira = null;
+        public event ImeCompositionEventHandler ImeCompositionKata = null;
+        public event ImeCompositionEventHandler ImeCompositedHira = null;
+        public event ImeCompositionEventHandler ImeCompositedKata = null;
+
+        // IMEでキーが押されたかのフラグ
+        private const int WM_IME_COMPOSITION = 0x010F;
+        // 変換確定後文字取得に使用する値(ひらがな)
+        private const int GCS_RESULTSTR = 0x0800;
+        // 変換確定後文字取得に使用する値(1バイトカタカナ)
+        private const int GCS_RESULTREADSTR = 0x0200;
+        // IME入力中文字取得に使用する値(ひらがな)
+        private const int GCS_COMPSTR = 0x0008;
+        // IME入力中文字取得に使用する値(1バイトカタカナ)
+        private const int GCS_COMPREADSTR = 0x0001;
+
+        [DllImport("Imm32.dll")]
+        private static extern int ImmGetContext(IntPtr hWnd);
+
+        [DllImport("Imm32.dll")]
+        private static extern int ImmGetCompositionString(
+            int hIMC, int dwIndex, StringBuilder lpBuf, int dwBufLen);
+
+        [DllImport("Imm32.dll")]
+        private static extern bool ImmReleaseContext(IntPtr hWnd, int hIMC);
+
+
+        protected override void WndProc(ref Message m)
+        {
+            // 変換確定(1バイトカタカナ)
+            if (ImeCompositedKata != null && m.Msg == WM_IME_COMPOSITION)
+            {
+                if (((int)m.LParam & GCS_RESULTREADSTR) > 0)
+                {
+                    int hIMC = ImmGetContext(this.Handle);
+                    int strLen = ImmGetCompositionString(hIMC, GCS_RESULTREADSTR, null, 0);
+                    StringBuilder str = new StringBuilder(strLen);
+
+                    ImmGetCompositionString(hIMC, GCS_RESULTREADSTR, str, str.Capacity);
+                    ImmReleaseContext(this.Handle, hIMC);
+
+                    if (ImeCompositedKata != null)
+                    {
+                        // 環境によって文字コードが違うので、それにあわせる
+                        byte[] tmp1 = System.Text.Encoding.Default.GetBytes(str.ToString());
+                        byte[] tmp2 = new byte[strLen];
+                        Array.Copy(tmp1, 0, tmp2, 0, strLen);
+                        ImeCompositedKata(this,
+                            new ImeCompositionEventArgs(System.Text.Encoding.Default.GetString(tmp2)));
+                    }
+                }
+            }
+
+            // 変換確定(ひらがな)
+            if (ImeCompositedHira != null && m.Msg == WM_IME_COMPOSITION)
+            {
+                if (((int)m.LParam & GCS_RESULTSTR) > 0)
+                {
+                    int hIMC = ImmGetContext(this.Handle);
+                    int strLen = ImmGetCompositionString(hIMC, GCS_RESULTSTR, null, 0);
+                    StringBuilder str = new StringBuilder(strLen);
+
+                    ImmGetCompositionString(hIMC, GCS_RESULTSTR, str, str.Capacity);
+                    ImmReleaseContext(this.Handle, hIMC);
+
+                    if (ImeCompositedHira != null)
+                    {
+                        // 環境によって文字コードが違うので、それにあわせる
+                        byte[] tmp1 = Encoding.Default.GetBytes(str.ToString());
+                        byte[] tmp2 = new byte[strLen];
+                        Array.Copy(tmp1, 0, tmp2, 0, strLen);
+                        ImeCompositedHira(this,
+                            new ImeCompositionEventArgs(Encoding.Default.GetString(tmp2)));
+                    }
+                }
+            }
+
+            // IMEで編集中の文字列 (1バイトカタカナ)
+            if (ImeCompositionKata != null && m.Msg == WM_IME_COMPOSITION)
+            {
+                if (((int)m.LParam & GCS_COMPREADSTR) > 0)
+                {
+                    int hIMC = ImmGetContext(this.Handle);
+                    int strLen = ImmGetCompositionString(hIMC, GCS_COMPREADSTR, null, 0);
+                    StringBuilder str = new StringBuilder(strLen);
+
+                    ImmGetCompositionString(hIMC, GCS_COMPREADSTR, str, str.Capacity);
+                    ImmReleaseContext(this.Handle, hIMC);
+
+                    if (ImeCompositionKata != null)
+                    {
+                        // 環境によって文字コードが違うので、それにあわせる
+                        byte[] tmp1 = Encoding.Default.GetBytes(str.ToString());
+                        byte[] tmp2 = new byte[strLen];
+                        Array.Copy(tmp1, 0, tmp2, 0, strLen);
+                        ImeCompositionKata(this,
+                            new ImeCompositionEventArgs(Encoding.Default.GetString(tmp2)));
+                    }
+                }
+            }
+
+            // IMEで編集中の文字列 (ひらがな)
+            if (ImeCompositionHira != null && m.Msg == WM_IME_COMPOSITION)
+            {
+                if (((int)m.LParam & GCS_COMPSTR) > 0)
+                {
+                    int hIMC = ImmGetContext(this.Handle);
+                    int strLen = ImmGetCompositionString(hIMC, GCS_COMPSTR, null, 0);
+                    StringBuilder str = new StringBuilder(strLen);
+
+                    ImmGetCompositionString(hIMC, GCS_COMPSTR, str, str.Capacity);
+                    ImmReleaseContext(this.Handle, hIMC);
+
+                    if (ImeCompositionHira != null)
+                    {
+                        // 環境によって文字コードが違うので、それにあわせる
+                        byte[] tmp1 = Encoding.Default.GetBytes(str.ToString());
+                        byte[] tmp2 = new byte[strLen];
+                        Array.Copy(tmp1, 0, tmp2, 0, strLen);
+                        ImeCompositionHira(this,
+                            new ImeCompositionEventArgs(Encoding.Default.GetString(tmp2)));
+                    }
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        [Serializable]
+        public class ImeCompositionEventArgs : EventArgs
+        {
+            private string str;
+            public ImeCompositionEventArgs(string str)
+            {
+                this.str = str;
+            }
+
+            public string InputString
+            {
+                set
+                {
+                    this.str = value;
+                }
+                get
+                {
+                    return this.str;
+                }
+            }
+        }
+    }
+}
