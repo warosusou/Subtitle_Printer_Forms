2c066ae068ad17b703bf52454f1e1ba1204b4e63
warosusou <warosusou@outlook.com>
2019-08-09 11:20:19 +0900

    通常文書の作成を完成

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,9 +29,17 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.Reference_TextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -39,43 +47,122 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(776, 31);
+            this.panel1.Size = new System.Drawing.Size(892, 31);
             this.panel1.TabIndex = 1;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(460, 6);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 16);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "右揃え";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(386, 6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 16);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "中央揃え";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(324, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "左揃え";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(90, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "印刷";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Reference_TextBox
             // 
             this.Reference_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.Reference_TextBox.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Reference_TextBox.Location = new System.Drawing.Point(12, 50);
             this.Reference_TextBox.Multiline = true;
             this.Reference_TextBox.Name = "Reference_TextBox";
-            this.Reference_TextBox.Size = new System.Drawing.Size(776, 388);
+            this.Reference_TextBox.Size = new System.Drawing.Size(892, 254);
             this.Reference_TextBox.TabIndex = 2;
             this.Reference_TextBox.Text = "Reference TextBox";
+            this.Reference_TextBox.WordWrap = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 310);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(892, 128);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "フォント";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "印刷用フォント";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(916, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Reference_TextBox);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +172,13 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox Reference_TextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,8 @@ namespace Subtitle_Printer
         ImeReadableTextBox textBox;
         Color TextColor;
         bool EQ;
+        int currentline = -1;
+        Font PrintingFont;
         public Form1()
         {
             InitializeComponent();
@@ -25,24 +27,72 @@ namespace Subtitle_Printer
                 Multiline = Reference_TextBox.Multiline,
                 Anchor = Reference_TextBox.Anchor,
                 Margin = Reference_TextBox.Margin,
-                Text = "ImeTextBox"
+                Text = "ImeTextBox",
+                WordWrap = Reference_TextBox.WordWrap,
+                Font = Reference_TextBox.Font
             };
             textBox.ImeCompositionHira += TextBox_ImeCompositionHira;
-            textBox.KeyDown += TextBox_KeyDown;
-            textBox.KeyPress += TextBox_KeyPress;
             textBox.TextChanged += TextBox_TextChanged;
+            textBox.KeyDown += TextBox_KeyDown;
+            textBox.KeyUp += TextBox_KeyUp;
+            textBox.KeyPress += TextBox_KeyPress;
+            textBox.Click += TextBox_Click;
             this.Controls.Add(textBox);
             Reference_TextBox.Visible = false;
             TextColor = textBox.SelectionColor;
+            PrintingFont = new Font("メイリオ", 20);
+            LineChangeDetector();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox.Font = fontDialog1.Font;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                PrintingFont.Dispose();
+                PrintingFont = fontDialog1.Font;
+                Print_Subtitle();
+            }
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            Print_Subtitle();
         }
 
         private void TextBox_TextChanged(object sender, EventArgs e)
         {
+            LineChangeDetector();
+        }
 
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right || e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Back || e.KeyCode == Keys.PageUp || e.KeyCode == Keys.PageDown || e.KeyCode == Keys.Home || e.KeyCode == Keys.End)
+            {                
+                var t = new Timer { Interval = 10, Enabled = true };
+                t.Tick += (s, ev) =>
+                  {
+                      t.Enabled = false;
+                      LineChangeDetector();
+                  };
+            }
+        }
+
+        private void TextBox_Click(object sender, EventArgs e)
+        {
+            LineChangeDetector();
         }
 
         private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
+            /*
             if (textBox.SelectionStart + "</Tex>".Length > textBox.Text.Length) { return; }
             if (e.KeyChar == '\\' && textBox.Text[textBox.SelectionStart - 1] == '\\' && textBox.Text.Substring(textBox.SelectionStart, "</Tex>".Length) == "</Tex>" && EQ)
             {
@@ -57,27 +107,31 @@ namespace Subtitle_Printer
             {
                 e.Handled = true;
             }
-            else if(EQ)
+            else if (EQ)
             {
                 var charactor = e.KeyChar;
                 e.Handled = true;
                 textBox.SelectedText = charactor.ToString();
             }
+            */
         }
 
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
+            /*
             if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right && EQ)
-            {
+            {*/
                 /*if (textBox.SelectionStart < EQ_head || EQ_last < ime.SelectionStart)
                 {
                     Leave_EQmode();
                 }*/
-            }
+            //}
+            
         }
 
         private void TextBox_ImeCompositionHira(object sender, ImeReadableTextBox.ImeCompositionEventArgs e)
         {
+            /*
             if (e.InputString == "￥" && !EQ)
             {
                 var pos = textBox.SelectionStart;
@@ -89,6 +143,7 @@ namespace Subtitle_Printer
                 textBox.SelectionColor = Color.Red;
                 this.Text += " - EQ mode";
             }
+            */
         }
         private void Leave_EQmode()
         {
@@ -97,6 +152,103 @@ namespace Subtitle_Printer
             textBox.SelectionColor = TextColor;
             textBox.ImeMode = ImeMode.Hiragana;
             this.Text = "Form1";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Save_Subtitles();
+        }
+
+        private void LineChangeDetector()
+        {
+            int currentline = 0;
+            int charactors = 0;
+            if(textBox.Lines.Length == 0) { Print_Subtitle(); return; }
+            while (true)
+            {
+                charactors += textBox.Lines[currentline].Length + 1;//+1は改行コード
+                if (charactors > textBox.SelectionStart) { break; }
+                else if(currentline >= textBox.Lines.Length -1) { break; }
+                currentline++;
+            }
+            if(currentline != this.currentline) { this.currentline = currentline; }
+            Print_Subtitle();
+        }
+
+        private void Print_Subtitle()
+        {
+            string text = "";
+            if (pictureBox1.Image != null)pictureBox1.Image.Dispose();
+            if (textBox.Lines.Length != 0)
+            {
+                int currentline = 0;
+                int charactors = 0;
+                while (true)
+                {
+                    charactors += textBox.Lines[currentline].Length + 1;//+1は改行コード
+                    if (charactors > textBox.SelectionStart) { break; }
+                    else if (currentline >= textBox.Lines.Length - 1) { break; }
+                    currentline++;
+                }
+                if (currentline < 0 || currentline >= textBox.Lines.Length) { return; }
+                text = textBox.Lines[currentline];
+                if (text.EndsWith(":")) text = "";
+                if (text.Contains("%")) text = text.Split('%')[0];
+            }
+            //PictureBox1に表示する
+            pictureBox1.Image = Graphicer(text);
+        }
+
+        private void Save_Subtitles()
+        {
+            for (int currentline = 0; currentline < textBox.Lines.Length; currentline++)
+            {
+                string text = "";
+                if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
+                if (textBox.Lines[currentline].Length != 0)
+                { 
+                    text = textBox.Lines[currentline];
+                    if (text.EndsWith(":")) continue;
+                    if (text.Contains("%")) text = text.Split('%')[0];
+                    Graphicer(text).Save(String.Format("Line{0}.bmp", currentline));
+                }
+            }
+        }
+
+        private Bitmap Graphicer(string text)
+        {
+            PointF pt = new PointF(0,pictureBox1.Height/2);
+            var strfmt = new StringFormat { Alignment = StringAlignment.Near,LineAlignment = StringAlignment.Center };
+            if (radioButton1.Checked)
+            {
+                strfmt.Alignment = StringAlignment.Near;
+            }
+            else if (radioButton2.Checked)
+            {
+                strfmt.Alignment = StringAlignment.Center;
+                pt = new PointF(pictureBox1.Width / 2, pictureBox1.Height/2);
+            }
+            else if (radioButton3.Checked)
+            {
+                strfmt.Alignment = StringAlignment.Far;
+                pt = new PointF(pictureBox1.Width, pictureBox1.Height/2);
+            }
+            //描画先とするImageオブジェクトを作成する
+            var canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            var g = Graphics.FromImage(canvas);
+            //g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            //フォントオブジェクトの作成
+            var fnt = new Font(PrintingFont.Name,PrintingFont.Size);
+            //文字列を位置pt、黒で表示
+            g.DrawString(text, fnt, Brushes.Black, pt,strfmt);
+            //リソースを解放する
+            strfmt.Dispose();
+            fnt.Dispose();
+            g.Dispose();
+            return canvas;
         }
     }
 }
